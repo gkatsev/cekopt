9ff7a37abdd8c2d217bd8575822617a3010a8b65
Gary Katsevman <git@gkatsev.com>
2015-02-27 00:05:21 -0500

    Add a simple asc/des toggle

--- app/app.js
+++ app/app.js
@@ -5,32 +5,46 @@ var request = require('request');
 var url = require('url');
 var document = require('global/document');
 var utils = require('../utils.js');
+var datalist = [];
 
-var displayList = function(list) {
+var orderingRadios = document.querySelector('.ordering-group');
+
+var displayList = function() {
   var ol = document.createElement('ol');
+  var placeholder = document.querySelector('.placeholder');
+  var ordering = document.querySelector('.ordering:checked').value;
 
-  list.sort(function(a, b) {
-    return a.readability.word_count - b.readability.word_count;
+
+  datalist.sort(function(a, b) {
+    if (ordering === 'asc') {
+      return a.readability.word_count - b.readability.word_count;
+    }
+
+    return b.readability.word_count - a.readability.word_count;
   }).forEach(function(item) {
-      var li = document.createElement('li');
-      var a = document.createElement('a');
+    var li = document.createElement('li');
+    var a = document.createElement('a');
 
-      a.textContent = item.resolved_title || item.readability.title || item.resolved_url;
-      a.href = item.resolved_url;
-      li.appendChild(document.createTextNode('['+ item.readability.word_count +'] '));
-      li.appendChild(document.createTextNode('['+ item.readability.word_count/250 +' mins] '));
-      li.appendChild(a);
-      ol.appendChild(li);
-
+    a.textContent = item.resolved_title || item.readability.title || item.resolved_url;
+    a.href = item.resolved_url;
+    li.appendChild(document.createTextNode('['+ item.readability.word_count +'] '));
+    li.appendChild(document.createTextNode('['+ item.readability.word_count/250 +' mins] '));
+    li.appendChild(a);
+    ol.appendChild(li);
   });
 
-  document.body.appendChild(ol);
+  while(placeholder.firstChild) {
+    placeholder.removeChild(placeholder.firstChild);
+  }
+  document.querySelector('.placeholder').appendChild(ol);
 }
 
 var doWork = function(json) {
-  var list = json.list
-  displayList(list);
+  datalist = json.list
+  displayList();
 };
+
+orderingRadios.addEventListener('click', displayList);
 
 request({
   url: '/pocket/list',
